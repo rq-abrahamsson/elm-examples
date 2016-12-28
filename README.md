dd1e74b369359349a43f1f90ff78e814170396f3
Robin Abrahamsson <robin.abrahamsson25@gmail.com>
2016-12-28 16:08:07 +0100

    Added http request example.

---
+++ README.md
@@ -0,0 +1,4 @@
+#Prepare
+
+1. Install [[https://github.com/boot-clj/boot][Boot]]
+2. Install JDK 1.7+
--- elm/src/elm/Main.elm
+++ elm/src/elm/Main.elm
@@ -3,6 +3,10 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Http
+import Json.Encode
+import Json.Decode
+import Json.Decode.Pipeline
 
 
 -- component import example
@@ -13,9 +17,14 @@ import Components.Hello exposing (hello)
 -- APP
 
 
-main : Program Never Int Msg
+main : Program Never Model Msg
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -23,12 +32,39 @@ main =
 
 
 type alias Model =
-    Int
+    { number : Int
+    , profile : Profile
+    }
 
 
-model : number
+type alias Profile =
+    { id : Int
+    }
+
+
+model : Model
 model =
-    0
+    { number = 0
+    , profile = { id = 1 }
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( model, getData )
+
+
+decodeProfile : Json.Decode.Decoder Profile
+decodeProfile =
+    Json.Decode.Pipeline.decode Profile
+        |> Json.Decode.Pipeline.required "id" (Json.Decode.int)
+
+
+encodeProfile : Profile -> Json.Encode.Value
+encodeProfile record =
+    Json.Encode.object
+        [ ( "id", Json.Encode.int <| record.id )
+        ]
 
 
 
@@ -37,17 +73,39 @@ model =
 
 type Msg
     = NoOp
+    | GetData
+    | NewData (Result Http.Error Profile)
     | Increment
 
 
-update : Msg -> Model -> Model
+getData =
+    let
+        url =
+            "http://localhost:3000/profile"
+
+        request =
+            Http.get url decodeProfile
+    in
+        Http.send NewData request
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model
+            ( model, Cmd.none )
+
+        GetData ->
+            ( model, getData )
+
+        NewData (Ok newData) ->
+            ( { model | profile = newData }, Cmd.none )
+
+        NewData (Err _) ->
+            ( model, Cmd.none )
 
         Increment ->
-            model + 1
+            ( { model | number = model.number + 1 }, Cmd.none )
 
 
 
@@ -60,13 +118,24 @@ view : Model -> Html Msg
 view model =
     div [ style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
         [ div []
-            [ hello model
+            [ hello model.number
+            , img [ src "static/img/elm.jpg", style styles.img ] []
             , p [] [ text "Elm" ]
-            , button [ class "btn btn-primary btn-lg", onClick Increment ]
+            , p [] [ text ("Logged in profile: " ++ toString model.profile.id) ]
+            , button [ class "btn btn-primary btn-lg", onClick GetData ]
                 [ span [] [ text "Button" ]
                 ]
             ]
         ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
